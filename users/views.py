f90ffcbe8aa45dcc7891f24df80f93e7b3235a5a
dr-zieba <zieba1jan@gmail.com>
2022-12-29 21:04:26 +0100

    user profile added

--- users/views.py
+++ users/views.py
@@ -6,3 +6,15 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+
+    #profile skills with description
+    topSkills = profile.skill_set.exclude(description__exact="")
+
+    #profile skill without description
+    otherSkills = profile.skill_set.filter(description="")
+
+    context = {'profile': profile, 'topSkills': topSkills, 'otherSkills': otherSkills}
+    return render(request, 'users/user-profile.html', context)
